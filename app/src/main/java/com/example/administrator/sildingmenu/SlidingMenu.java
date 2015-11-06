532dab3de69940d2ea8ee3141accfbd002ef0fc9
BigBoss <384214402@qq.com>
2015-11-06 17:17:15 +0800

    20151106

--- app/src/main/java/com/example/administrator/sildingmenu/SlidingMenu.java
+++ app/src/main/java/com/example/administrator/sildingmenu/SlidingMenu.java
@@ -1,8 +1,10 @@
 package com.example.administrator.sildingmenu;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.util.AttributeSet;
 import android.util.TypedValue;
+import android.view.MotionEvent;
 import android.view.ViewGroup;
 import android.widget.HorizontalScrollView;
 import android.widget.LinearLayout;
@@ -11,27 +13,80 @@ import android.widget.LinearLayout;
  * Created by Administrator on 2015/11/12.
  */
 public class SlidingMenu extends HorizontalScrollView {
-
     private int mScreenWidth;
-    private int mMenuRightPadding =50;
+    private int mMenuRightPadding;
     private int mMenuWidth;
     private int mHalfMenuWidth;
     private boolean once;
 
+    public SlidingMenu(Context context) {
+        this(context, null, 0);
+    }
     public SlidingMenu(Context context, AttributeSet attrs) {
-        super(context, attrs);
-//        mScreenWidth = Screenu
+        this(context, attrs, 0);
+    }
+
+    public SlidingMenu(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        mScreenWidth = ScreenUtils.getScreenWidth(context);
+
+        TypedArray a = context.getTheme().obtainStyledAttributes(attrs, R.styleable.SlidingMenu,
+                defStyle, 0);
+        int n = a.getIndexCount();
+        for (int i = 0; i < n; i++) {
+            int attr = a.getIndex(i);
+            switch (attr) {
+                case R.styleable.SlidingMenu_rightPadding:
+                    mMenuRightPadding = a.getDimensionPixelSize(attr, (int) TypedValue
+                            .applyDimension(TypedValue.COMPLEX_UNIT_DIP, 50f, getResources()
+                                    .getDisplayMetrics()));
+//                    getDimension如果是dimen是dp或sp的单位,将其乘以density,如果是px,
+//                    则不乘，返回类型为float。getDimensionPixelSize总体类似，但是当单位是px时，也会乘以density，返回类型为int。
+                    break;
+            }
+        }
+        a.recycle();
+
+
     }
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         if (!once) {
+
             LinearLayout wrapper = (LinearLayout) getChildAt(0);
             ViewGroup menu = (ViewGroup) wrapper.getChildAt(0);
             ViewGroup content = (ViewGroup) wrapper.getChildAt(1);
 
-//            mMenuRightPadding=(int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,m)
+            mMenuWidth = mScreenWidth - mMenuRightPadding;
+            mHalfMenuWidth = mMenuWidth / 2;
+            menu.getLayoutParams().width = mMenuWidth;
+            content.getLayoutParams().width = mScreenWidth;
+
         }
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+        super.onLayout(changed, l, t, r, b);
+        if (changed) {
+            this.scrollTo(mMenuWidth, 0);
+            once = true;
+        }
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent ev) {
+        int action = ev.getAction();
+        switch (action) {
+            case MotionEvent.ACTION_UP:
+                int scrollX = getScrollX();
+                if (scrollX > mHalfMenuWidth)
+                    this.smoothScrollTo(mMenuWidth, 0);
+                else
+                    this.smoothScrollTo(0, 0);
+        }
+        return super.onTouchEvent(ev);
     }
 }
